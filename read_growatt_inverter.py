c7045547ee647b8296f87e99dcde4cb863044d10
Heleen <git@heyl1.com>
2019-02-24 13:05:35 +0100

    Fix tabs/spaces

--- read_growatt_inverter.py
+++ read_growatt_inverter.py
@@ -54,11 +54,11 @@ class GracefulKiller:
     kill_now = False
 
     def __init__(self):
-	signal.signal(signal.SIGINT, self.exit_gracefully)
-	signal.signal(signal.SIGTERM, self.exit_gracefully)
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
 
     def exit_gracefully(self,signum, frame):
-	self.kill_now = True
+        self.kill_now = True
 
 
 def get_lock(process_name):
